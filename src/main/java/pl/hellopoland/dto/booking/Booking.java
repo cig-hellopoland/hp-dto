a037d47e5d6be81707e5e629999eb5075e3d1282
Dawid Olejnik <dawid@fream.pl>
2018-06-08 09:12:38 +0200

    0.3.2-SNAPSHOT - changed collection to array

--- src/main/java/pl/hellopoland/dto/booking/Booking.java
+++ src/main/java/pl/hellopoland/dto/booking/Booking.java
@@ -1,9 +1,7 @@
 package pl.hellopoland.dto.booking;
 
-import java.util.Collection;
-
 public class Booking {
   public String customerName;
   public String customerEmail;
-  public Collection<Ticket> ticketBookings;
+  public Ticket[] ticketBookings;
 }
