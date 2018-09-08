67ed54188cc1eef61706fc95b39757f95738af0a
lmrozek <lmrozek@fream.pl>
2018-09-08 10:58:51 +0200

    hpl-387

---
+++ src/main/java/pl/hellopoland/dto/AvailableTicketNumberAssociationDTO.java
@@ -0,0 +1,8 @@
+package pl.hellopoland.dto;
+
+public class AvailableTicketNumberAssociationDTO {
+  public Integer availableTicketsNumber;
+  public Long ticketDefinitionId;
+  public Long ticketPoolDefinitionId;
+  public Long ticketPoolId;
+}
