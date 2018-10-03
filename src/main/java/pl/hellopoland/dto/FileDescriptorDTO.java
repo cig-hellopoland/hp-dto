2899d9ff10554fa795f05072ac34ff7926fa2f14
lmrozek <lmrozek@fream.pl>
2018-10-03 13:05:17 +0200

    hpl-416 FileDescriptorDTO

---
+++ src/main/java/pl/hellopoland/dto/FileDescriptorDTO.java
@@ -0,0 +1,10 @@
+package pl.hellopoland.dto;
+
+import java.time.LocalDateTime;
+
+public class FileDescriptorDTO {
+  public Long id;
+  public String path;
+  public LocalDateTime created;
+  public String type;
+}
