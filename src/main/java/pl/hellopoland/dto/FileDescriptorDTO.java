3c583016329358fd55ac3d962b259a3a094259dc
lmrozek <lmrozek@fream.pl>
2019-01-14 11:00:21 +0100

    hpl-553 added downloadUrl into FileDescriptorDTO

--- src/main/java/pl/hellopoland/dto/FileDescriptorDTO.java
+++ src/main/java/pl/hellopoland/dto/FileDescriptorDTO.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 public class FileDescriptorDTO extends DTOSuperclass {
   public Long id;
   public String path;
+  public String downloadUrl;
   public LocalDateTime created;
   public String type;
   public String name;
