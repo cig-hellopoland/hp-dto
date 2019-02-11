c108da5714f69193df3ff2218477d53c2ac830a1
lmrozek <lmrozek@fream.pl>
2019-02-11 15:04:36 +0100

    hpl-622

--- src/main/java/pl/hellopoland/dto/UserAuthDTO.java
+++ src/main/java/pl/hellopoland/dto/UserAuthDTO.java
@@ -4,6 +4,7 @@ public class UserAuthDTO extends DTOSuperclass {
 
   public String login;
   public String password;
+  public String oldPassword;
   public String accessToken;
   public String refreshToken;
 
