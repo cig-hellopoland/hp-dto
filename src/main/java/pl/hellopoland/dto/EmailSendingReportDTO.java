f98714b3f07a5678300b83e6fff697c5a8f411e6
lmrozek <lmrozek@fream.pl>
2019-04-30 14:02:25 +0200

    hpl-753 + hpl-682(webp)

---
+++ src/main/java/pl/hellopoland/dto/EmailSendingReportDTO.java
@@ -0,0 +1,7 @@
+package pl.hellopoland.dto;
+
+public class EmailSendingReportDTO {
+  public String[] validSentAddresses;
+  public String[] validUnsentAddresses;
+  public String[] invalidAddresses;
+}
--- src/main/java/pl/hellopoland/dto/ImageDTO.java
+++ src/main/java/pl/hellopoland/dto/ImageDTO.java
@@ -3,12 +3,20 @@ package pl.hellopoland.dto;
 public class ImageDTO extends DTOSuperclass {
 
   public String qvg;
+  public String qvgWebp;
   public String vga;
+  public String vgaWebp;
   public String hd;
+  public String hdWebp;
   public String xga;
+  public String xgaWebp;
   public String sxga;
+  public String sxgaWebp;
   public String fhd;
+  public String fhdWebp;
   public String fourK;
+  public String fourKWebp;
   public String original;
+  public String originalWebp;
 
 }
