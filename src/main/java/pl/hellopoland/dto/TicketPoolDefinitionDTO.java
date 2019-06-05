f0d3a580ae5efc499635080e9e376defd0e6b173
Dawid Olejnik <dawid@fream.pl>
2019-06-05 19:15:17 +0200

    deep copy

--- src/main/java/pl/hellopoland/dto/TicketPoolDefinitionDTO.java
+++ src/main/java/pl/hellopoland/dto/TicketPoolDefinitionDTO.java
@@ -1,5 +1,6 @@
 package pl.hellopoland.dto;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -29,5 +30,20 @@ public class TicketPoolDefinitionDTO extends DTOSuperclass {
 
   public boolean deleted;
   public boolean wholeDay;
+
+  @Override
+  public Object clone() throws CloneNotSupportedException {
+    TicketPoolDefinitionDTO copy = (TicketPoolDefinitionDTO) super.clone();
+    if (this.frequencyData != null) {
+      copy.frequencyData = (FrequencyDataDTO) this.frequencyData.clone();
+    }
+    if (this.ticketDefinitions != null) {
+      copy.ticketDefinitions = new ArrayList<>();
+      for (TicketDefinitionDTO td : this.ticketDefinitions) {
+        copy.ticketDefinitions.add((TicketDefinitionDTO) td.clone());
+      }
+    }
+    return copy;
+  }
 
 }
