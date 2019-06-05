c9df76f2e8b2df0c52bd42eb1d6c8610d013041e
Dawid Olejnik <dawid@fream.pl>
2019-06-05 18:14:09 +0200

    1.0.27 cloneable

--- src/main/java/pl/hellopoland/dto/DTOSuperclass.java
+++ src/main/java/pl/hellopoland/dto/DTOSuperclass.java
@@ -1,5 +1,9 @@
 package pl.hellopoland.dto;
 
-public abstract class DTOSuperclass {
+public abstract class DTOSuperclass implements Cloneable {
 
+  @Override
+  public Object clone() throws CloneNotSupportedException {
+    return super.clone();
+  }
 }
