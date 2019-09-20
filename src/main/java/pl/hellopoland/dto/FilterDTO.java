7f7886ac4675dae3baf795503c211232a34d726c
Dawid Olejnik <dawid@fream.pl>
2019-09-20 09:36:58 +0200

    fromDate i toDate

--- src/main/java/pl/hellopoland/dto/FilterDTO.java
+++ src/main/java/pl/hellopoland/dto/FilterDTO.java
@@ -1,14 +1,17 @@
 package pl.hellopoland.dto;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FilterDTO {
 
   public String query = "";
+  public LocalDate fromDate = LocalDate.now();
+  public LocalDate toDate = LocalDate.now().plusDays(30);
   public List<String> city = new ArrayList<>();
-  public List<CategoryDTO> categories = new ArrayList<>();
-  public List<CategoryDTO> tags = new ArrayList<>();
   public List<FilterPriceEntryDTO> prices = new ArrayList<>();
+  public List<CategoryDTO> tags = new ArrayList<>();
+  public List<CategoryDTO> categories = new ArrayList<>();
 
 }
