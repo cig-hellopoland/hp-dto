35836393cf524a06e4d558974861bfbed096e5bb
Dawid Olejnik <dawid@fream.pl>
2019-09-23 09:38:19 +0200

    1.0.40-SNAPSHOT market partner

---
+++ src/main/java/pl/hellopoland/dto/MarketPartnerDTO.java
@@ -0,0 +1,16 @@
+package pl.hellopoland.dto;
+
+import java.util.List;
+
+public class MarketPartnerDTO extends DTOSuperclass {
+
+  public String name;
+  public List<CategoryDTO> categories;
+  public ImageDTO mainImage;
+  public List<String> cities;
+  public String description;
+  public LocationDTO correspondenceAddress;
+  public List<SightDTO> sights;
+  public List<SightEventDTO> sightEvents;
+
+}
