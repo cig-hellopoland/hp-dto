eecc0156dfd5b61ae46090da97b657e62893d517
wiktor <wwolakksiazek@fream.pl>
2019-10-11 12:16:59 +0200

    creating and editing user info

---
+++ src/main/java/pl/hellopoland/dto/UserInfoDTO.java
@@ -0,0 +1,14 @@
+package pl.hellopoland.dto;
+
+public class UserInfoDTO extends UserDTO {
+  public String firstName;
+  public String lastName;
+
+  public String phone;
+  public String street;
+  public String zipCode;
+  public String city;
+  public String country;
+
+
+}
