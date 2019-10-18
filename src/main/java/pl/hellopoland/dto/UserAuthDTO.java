2ad77c1d8135d37125d32a55ace74d04d7a2f033
wiktor <wwolakksiazek@fream.pl>
2019-10-18 15:58:16 +0200

    added password confirmation in userAuthDTO

--- src/main/java/pl/hellopoland/dto/UserAuthDTO.java
+++ src/main/java/pl/hellopoland/dto/UserAuthDTO.java
@@ -4,6 +4,7 @@ public class UserAuthDTO extends DTOSuperclass {
 
   public String login;
   public String password;
+  public String passwordConfirmation;
   public String oldPassword;
   public String accessToken;
   public String refreshToken;
