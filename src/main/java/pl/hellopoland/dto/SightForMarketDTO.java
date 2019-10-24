723a8f61693bed912cfbd0f1d45b77d1f1a44d44
wiktor <wwolakksiazek@fream.pl>
2019-10-24 12:36:56 +0200

    1.1.6 - Sight for market

---
+++ src/main/java/pl/hellopoland/dto/SightForMarketDTO.java
@@ -0,0 +1,33 @@
+package pl.hellopoland.dto;
+
+import java.util.List;
+import java.util.Set;
+
+public class SightForMarketDTO {
+  public Long id;
+  public String name;
+  public String lead;
+  public String description;
+  public Float score;
+  public ImageDTO mainImage;
+  public List<ImageDTO> images;
+  public String email;
+  public String phone;
+  public Boolean generalAdmission;
+  public LocationDTO location;
+  public List<SightEventForMarketDTO> sightEvents;
+  public List<OpeningHoursDTO> openingHours;
+  public List<AgreementDTO> agreements;
+  public Boolean published;
+  public Boolean blocked;
+  public Integer minPrice;
+  public String defaultLanguage;
+  public Set<String> availableLanguageVersions;
+  public String language;
+  public Set<CategoryDTO> categories;
+  public Set<TagDTO> tags;
+  public List<SightDTO> similar;
+  public Long partnerId;
+  public String partnerName;
+  public boolean favourite;
+}
