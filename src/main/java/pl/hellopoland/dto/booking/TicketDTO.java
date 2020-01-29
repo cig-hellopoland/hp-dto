88c13263e3906e1eea02b3bdfd778d736edee35b
Dawid Olejnik <dawid@fream.pl>
2020-01-29 14:16:57 +0100

    ticketDTO.discount

--- src/main/java/pl/hellopoland/dto/booking/TicketDTO.java
+++ src/main/java/pl/hellopoland/dto/booking/TicketDTO.java
@@ -3,10 +3,12 @@ package pl.hellopoland.dto.booking;
 import java.io.ByteArrayOutputStream;
 import java.util.Date;
 import pl.hellopoland.dto.DTOSuperclass;
+import pl.hellopoland.dto.DiscountDTO;
 import pl.hellopoland.dto.StatusDTO;
 import pl.hellopoland.dto.TicketPoolInfoDTO;
 
 public class TicketDTO extends DTOSuperclass {
+
   public Long id;
   public String name;
   public Integer price;
@@ -19,8 +21,7 @@ public class TicketDTO extends DTOSuperclass {
   public Long bookingId;
   public BookingDTO booking;
   public boolean wholeDay;
-
+  public DiscountDTO discount;
   public TicketPoolInfoDTO ticketPoolInfo;
-
 
 }
