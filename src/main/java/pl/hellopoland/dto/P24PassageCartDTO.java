491646ad1ec5dd42ea70a3806a1f7c504c3245b1
Dawid Olejnik <dawid@fream.pl>
2021-01-27 11:16:38 +0100

    1.2.8 p24passageCart.requiresPayment

--- src/main/java/pl/hellopoland/dto/P24PassageCartDTO.java
+++ src/main/java/pl/hellopoland/dto/P24PassageCartDTO.java
@@ -1,6 +1,7 @@
 package pl.hellopoland.dto;
 
 public class P24PassageCartDTO extends DTOSuperclass {
+  public boolean requiresPayment = true;
   public boolean isSandbox;
   public P24PassageTransactionParamsDTO transactionParams;
 }
