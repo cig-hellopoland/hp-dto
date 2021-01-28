c967be07dd641ca19bc7dfe5f0670d1db53263f2
Dawid Olejnik <dawid@fream.pl>
2021-01-28 15:29:19 +0100

    1.2.9 p24passageCart.orderId and orderHash

--- src/main/java/pl/hellopoland/dto/P24PassageCartDTO.java
+++ src/main/java/pl/hellopoland/dto/P24PassageCartDTO.java
@@ -2,6 +2,8 @@ package pl.hellopoland.dto;
 
 public class P24PassageCartDTO extends DTOSuperclass {
   public boolean requiresPayment = true;
+  public Long orderId;
+  public String orderHash;
   public boolean isSandbox;
   public P24PassageTransactionParamsDTO transactionParams;
 }
